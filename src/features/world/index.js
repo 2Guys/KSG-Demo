5b5d9521ce7389a137e2edab98aed2a20d6ce548
Greashun <vncntts@gmail.com>
2019-11-16 21:46:00 -0500

    Enter

--- src/features/world/index.js
+++ src/features/world/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Player from '../player'
 // import Enemy from '../enemy'
 import Map from '../map'
-import Sky from '../sky'
 
 const World = (props) => {
   return (  
     <div>
-      {/* <Sky /> */}
       <Map />
       <Player />
       {/* <Enemy /> */}
