137cebebab3c5a76976b26e55eaa2c5bfd4cca2d
Sayeed Joseph <sayeedjoseph@Sayeeds-Air.home>
2019-11-23 22:29:39 -0500

    fix: spelling mistake in paragraph

--- src/features/home/home.js
+++ src/features/home/home.js
@@ -11,8 +11,8 @@ function Home (props) {
                 <h1>KIDS SEE GHOST</h1>
                 <h5>Demo</h5>
                 <h4>By: Sayeed Joseph & Vincent Tse</h4>
-                <p>this is a demo of the Kids See Ghost the game. 
-                It is based on the album Kids See Ghost by Kid Cuid and Kanye West.</p>
+                <p>This is a demo of the Kids See Ghost the game. 
+                It is based on the album Kids See Ghost by Kid Cudi and Kanye West.</p>
                 <Link to='/world'>
                     <button> Start</button>
                 </Link>
