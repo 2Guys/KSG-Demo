a71ddb396cce9ab82a8990a6df38b370bbaa441a
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-04-26 17:38:00 -0400

    chore: update README.md

--- src/features/home/home.js
+++ src/features/home/home.js
@@ -7,7 +7,7 @@ function Home (props) {
             <h1>KIDS SEE GHOST</h1>
             <h5>Demo</h5>
             <h4>By: Sayeed Joseph & Vincent Tse</h4>
-            <p>this is a demo of the Kids See Ghost the game. 
+            <p>This is a demo of Kids See Ghost the game. 
             It is based on the album Kids See Ghost by Kid Cudi and Kanye West.</p>
             <Link to='/world'>
                 <button> Start</button>
